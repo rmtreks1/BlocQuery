f60a3a988b53c2a32ebf19d64ff12ca21f7220d7
Roshan Mahanama <rmtreks@gmail.com>
2015-05-02 14:09:55 +1000

    Questions PFObject set up

---
+++ BlocQuery/BlocQuery/Questions.h
@@ -0,0 +1,15 @@
+//
+//  Questions.h
+//  BlocQuery
+//
+//  Created by Roshan Mahanama on 2/05/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface Questions : PFObject <PFSubclassing>
++ (NSString *)parseClassName;
+
+
+@end
---
+++ BlocQuery/BlocQuery/Questions.m
@@ -0,0 +1,27 @@
+//
+//  Questions.m
+//  BlocQuery
+//
+//  Created by Roshan Mahanama on 2/05/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+#import "Questions.h"
+#import <Parse/PFObject+Subclass.h>
+
+@implementation Questions
+
++ (void)load {
+    [self registerSubclass];
+}
+
++ (NSString *)parseClassName {
+    return @"Questions";
+}
+
+
+
+
+
+
+@end
