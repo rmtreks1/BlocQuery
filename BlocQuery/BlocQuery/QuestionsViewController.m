df6b64aebf8baeebfeb5a0908175969a3e0d7512
Roshan Mahanama <rmtreks@gmail.com>
2015-05-03 18:10:37 +1000

    login detected on app launch

--- BlocQuery/BlocQuery/QuestionsViewController.m
+++ BlocQuery/BlocQuery/QuestionsViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "QuestionsViewController.h"
+#import <Parse/Parse.h>
+#import "LoginViewController.h"
+#import "SignUpViewController.h"
 
-@interface QuestionsViewController ()
+
+
+@interface QuestionsViewController () <PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate>
+@property (nonatomic, strong) PFUser *currentUser;
 
 @end
 
@@ -18,10 +24,20 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-//    // testing parse
-//    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
-//    testObject[@"foo"] = @"bar";
-//    [testObject saveInBackground];
+    if (!self.currentUser) {
+        NSLog(@"Not logged in");
+        
+        // testing the login controller
+        LoginViewController *loginController = [[LoginViewController alloc] init];
+        loginController.delegate = self;
+        
+        // custom signup controller
+        loginController.signUpController = [[SignUpViewController alloc] init];
+        loginController.signUpController.delegate = self;
+        
+        [self presentViewController:loginController animated:YES completion:nil];
+    }
+    
     
     
 }
@@ -30,5 +46,37 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+
+#pragma mark - Logging in to Parse
+// move this code to button or screen that triggers the login
+
+- (void)logInViewController:(PFLogInViewController *)controller
+               didLogInUser:(PFUser *)user {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)logInViewControllerDidCancelLogIn:(PFLogInViewController *)logInController {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+- (void)signUpViewController:(PFSignUpViewController *)signUpController didSignUpUser:(PFUser *)user {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)signUpViewControllerDidCancelSignUp:(PFSignUpViewController *)signUpController {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+
+
+
+
 
 @end
