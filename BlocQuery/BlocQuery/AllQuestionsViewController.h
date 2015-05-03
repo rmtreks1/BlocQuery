b49f0385226327885f5486cdca5b5a1b52975051
Roshan Mahanama <rmtreks@gmail.com>
2015-05-04 09:48:08 +1000

    allQuestionsTableViewController - retrieving data from Parse

---
+++ BlocQuery/BlocQuery/AllQuestionsViewController.h
@@ -0,0 +1,15 @@
+//
+//  AllQuestionsViewController.h
+//  BlocQuery
+//
+//  Created by Roshan Mahanama on 4/05/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+#import <ParseUI/ParseUI.h>
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface AllQuestionsViewController : PFQueryTableViewController
+
+@end
