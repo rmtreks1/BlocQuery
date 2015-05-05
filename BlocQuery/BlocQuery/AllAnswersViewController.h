e48dfbcc9acdae98a878c9a6997e0f5c796ebb25
Roshan Mahanama <rmtreks@gmail.com>
2015-05-06 09:12:57 +1000

    Answers view controller hooked up - no query attached yet.

---
+++ BlocQuery/BlocQuery/AllAnswersViewController.h
@@ -0,0 +1,15 @@
+//
+//  AllAnswersViewController.h
+//  BlocQuery
+//
+//  Created by Roshan Mahanama on 6/05/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+#import <ParseUI/ParseUI.h>
+#import <Parse/Parse.h>
+#import <UIKit/UIKit.h>
+
+@interface AllAnswersViewController : PFQueryTableViewController
+
+@end
